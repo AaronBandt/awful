bbd66a5f9e5f13b7d1f4676f35e1053e873d3127
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-03-17 09:30:39 -0700

    removing dependency_links

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(name='awfulweb',
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
-      dependency_links = ['http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-1.2.3.zip#md5=6d42998cfec6e85b902d4ffa5a35ce86'],
       tests_require=requires,
       test_suite="awfulweb",
       entry_points="""\
