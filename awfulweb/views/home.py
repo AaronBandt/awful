608d832d34b9b1b2438e0ee166a23c12c5328a0f
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-03-24 22:46:07 -0700

    got rid of calling the cs api for point radius search

--- awfulweb/views/home.py
+++ awfulweb/views/home.py
@@ -3,13 +3,10 @@ from pyramid.response import Response
 from datetime import datetime
 from datetime import timedelta
 import arrow
-import urllib
 from awfulweb.views import (
     get_authenticated_user,
     site_layout,
     log,
-    _cs_api_query,
-    SearchResult,
     )
 from awfulweb.models import (
     DBSession,
@@ -41,78 +38,71 @@ def _place_response(name, place_id, avg, pa):
 def view_home(request):
     page_title = 'I ate lunch once. It was AWFUL.'
     au = get_authenticated_user(request)
+    visit_threshold = int(request.registry.settings['awful.visit_threshold'])
     has_reviews = False
     display = False
     results = False
-    # number in days before next visit allowed.
-    visit_threshold = 5
     places_response = []
     geo_places_ids = []
-    cs_pub_code = request.registry.settings['awful.cs_pub_code']
     awfulites = []
-#    awfulites = ['aaron.bandt@citygridmedia.com', 'user1@aaronbandt.com', 'julie@gmail.com']
+
 
     if 'whereto.submitted' in request.POST:
         awfulites = request.POST.getall('awfulite')
         display = True
 
+        # convert users to ids
+        awfulite_ids = []
+        for a in awfulites:
+            try:
+                q = DBSession.query(User).filter(User.user_name==a)
+                result = q.one()
+                awfulite_ids.append(result.user_id)
+            except Exception, e:
+                conn_err_msg = e
+                return Response(str(conn_err_msg), content_type='text/plain', status_int=500)
+
         try:
             # get these settings from the form or fallback to config defaults
             # FIXME: should be able to move this to the main function with some creativity
             try:
-                home_lat = request.POST['home_lat']
-                home_lon = request.POST['home_lon']
+                home_lat = float(request.POST['home_lat'])
+                home_lon = float(request.POST['home_lon'])
                 log.info("Got lat: %s lon: %s from browser" % (home_lat,home_lon))
             except:
-                home_lat = request.registry.settings['awful.default_lat']
-                home_lon = request.registry.settings['awful.default_lon']
+                home_lat = float(request.registry.settings['awful.default_lat'])
+                home_lon = float(request.registry.settings['awful.default_lon'])
                 pass
 
             try:
-                radius = request.POST['radius']
+                radius = float(request.POST['radius'])
             except:
-                radius = request.registry.settings['awful.default_radius']
+                radius = float(request.registry.settings['awful.default_radius'])
                 pass
 
-            s = {'lat': home_lat, 'lon': home_lon, 'radius': radius}
-            api_endpoint = '/content/places/v2/search/latlon?&format=json&rpp=50&type=restaurant&publisher=' + cs_pub_code
-            req = api_endpoint + '&' + urllib.urlencode(s)
-            resp = _cs_api_query(req)
+            # Find everything that's close
+            query = """
+                    SELECT cs_id, (
+                                   3959 *
+                                   acos( cos( radians( %(lat)f ) ) *
+                                   cos( radians( lat ) ) *
+                                   cos( radians( lon ) -
+                                   radians( %(lon)f ) ) +
+                                   sin( radians( %(lat)f ) ) *
+                                   sin( radians( lat ) ) ) )
+                    AS distance
+                    FROM places HAVING distance < %(dist)f
+                    """ % {'lat': home_lat, 'lon': home_lon, 'dist': radius}
+            near_results = DBSession.query('cs_id').from_statement(query).all()
+            # FIXME: one day I will understand why this is giving me a keyed tuple
+            for r in near_results:
+                geo_places_ids.append(r[0])
 
-            print "Number of responses: ", resp['results']['total_hits']
-            total_pages = (resp['results']['total_hits'] / 50) + 1
-            print "Total pages: ", total_pages
-            for r in resp['results']['locations']:
-                geo_places_ids.append(r['id'])
-
-            # get the rest
-            page = 2
-            while page <= total_pages:
-                new_req = req + '&page=' + str(page)
-                log.info("requesting page: %i" % (page))
-                resp = _cs_api_query(new_req)
-                for r in resp['results']['locations']:
-                    geo_places_ids.append(r['id'])
-                page += 1
-
-#            geo_places_ids = ['613773250', '731112440']
             q = DBSession.query(Place).filter(Place.cs_id.in_(geo_places_ids))
             places = q.all()
             for p in places:
-                print "Name: ", p.name
-                print "CS ID: ", p.cs_id
+                log.info("Found place: %s CS_ID: %s" % (p.name, p.cs_id))
 
-            # convert users to ids
-            awfulite_ids = []
-            for a in awfulites:
-                try:
-                    q = DBSession.query(User).filter(User.user_name==a)
-                    result = q.one()
-                    awfulite_ids.append(result.user_id)
-                except Exception, e:
-                    conn_err_msg = e
-                    return Response(str(conn_err_msg), content_type='text/plain', status_int=500)
-    
             # Check the places and remove ones that have a last visit date
             # less than the threshold
             for p in places:
@@ -148,7 +138,7 @@ def view_home(request):
     
             # Sort by rating
             places_response.sort(key=lambda x: x.avg, reverse=True)
-    
+
         except Exception, e:
             conn_err_msg = e
             return Response(str(conn_err_msg), content_type='text/plain', status_int=500)
@@ -159,7 +149,6 @@ def view_home(request):
 
         # Add a last visit record for each awfulite
         for a in awfulites:
- 
             try:
                 q = DBSession.query(User).filter(User.user_name==a)
                 result = q.one()
@@ -181,7 +170,6 @@ def view_home(request):
                 except Exception, e:
                     conn_err_msg = e
                     return Response(str(conn_err_msg), content_type='text/plain', status_int=500)
-
             else:
                 try:
                     log.info("Adding last_visit record for awfulite: %s for place_id: %s" % (a, place_id))
