52e6d1f9fbd59b577b96b41a33147fb3c10b3c37
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-03-25 23:02:03 -0700

    sorting and pagination

--- awfulweb/views/ratings.py
+++ awfulweb/views/ratings.py
@@ -18,6 +18,21 @@ from awfulweb.models import (
 def view_ratings(request):
     page_title = 'Your AWFUL ratings.'
     au = get_authenticated_user(request)
+    perpage = 10
+
+    params = {'sort_type': 'a',
+              'sort_order': 'a',
+              'start': 0
+             }
+    for p in params:
+        try:
+            params[p] = request.params[p]
+        except:
+            pass
+
+    sort_type = params['sort_type']
+    sort_order = params['sort_order']
+    offset = int(params['start'])
 
     if 'rating.submitted' in request.POST:
         place_id = request.POST['place_id']
@@ -47,12 +62,29 @@ def view_ratings(request):
     try:
         q = DBSession.query(Rating)
         q = q.filter(Rating.updated_by==au['login'])
-        rated = q.all()
-        rated.sort(key=lambda x: x.place.name, reverse=False)
+        q = q.join(Place)
+        total_rated = q.count()
+        # FIXME: feels like this should be better
+        if sort_type == 'a':
+            if sort_order == 'd':
+                rated = q.order_by(Place.name.desc()).limit(perpage).offset(offset)
+            else:
+                rated = q.order_by(Place.name.asc()).limit(perpage).offset(offset)
+        else:
+            if sort_order == 'd':
+                rated = q.order_by(Rating.rating.desc()).limit(perpage).offset(offset)
+            else:
+                rated = q.order_by(Rating.rating.asc()).limit(perpage).offset(offset)
+
 
         q = DBSession.query(Place).filter(~exists().where(and_(Place.place_id == Rating.place_id, Rating.updated_by == au['login'])))
-        unrated = q.all()
-        unrated.sort(key=lambda x: x.name, reverse=False)
+        total_unrated = q.count()
+        if sort_order == 'd':
+            unrated = q.order_by(Place.name.desc()).limit(perpage).offset(offset)
+        else:
+            unrated = q.order_by(Place.name.asc()).limit(perpage).offset(offset)
+
+#        unrated.sort(key=lambda x: x.name, reverse=False)
 
     except Exception, e:
         conn_err_msg = e
@@ -61,6 +93,12 @@ def view_ratings(request):
     return {'layout': site_layout(),
             'page_title': page_title,
             'au': au,
+            'perpage': perpage,
+            'offset': offset,
+            'sort_type': sort_type,
+            'sort_order': sort_order,
+            'total_rated': total_rated,
+            'total_unrated': total_unrated,
             'rated': rated,
             'unrated': unrated,
            }
