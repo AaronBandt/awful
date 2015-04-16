8e7144aeeac3b7940bfd6feded7fffcda38d9ac0
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-04-16 11:43:10 -0700

    noting to see here

--- awfulweb/views/test.py
+++ awfulweb/views/test.py
@@ -22,16 +22,39 @@ def view_test(request):
     page_title = 'test.'
     au = get_authenticated_user(request)
     results = False
+    params = {'type': 'vir',
+             }
+    for p in params:
+        try:
+            params[p] = request.params[p]
+        except:
+            pass
 
-    results=['Cafe Primo',
-             'Tender Greens',
-             'Five Guys',
-             'Burger Lounge'
-            ]
+    type = params['type']
+    if type == 'ec2':
+        host = 'aws1prdtcw1.opsprod.ctgrd.com'
+        uniq_id = 'i-303a6c4a'
+        ng = 'tcw'
+        vhost = 'aws1'
+    elif type == 'rds':
+        host = 'aws1devcpd1.csqa.ctgrd.com'
+        uniq_id = 'aws1devcpd1.cltftmkcg4dd.us-east-1.rds.amazonaws.com'
+        ng = 'none'
+        vhost = 'aws1'
+    else:
+        host = 'vir1prdpaw1.prod.cs'
+        uniq_id = '6A:37:2A:68:E1:B0'
+        ng = 'paw'
+        vhost = 'vir1prdxen41.prod.cs'
 
     return {'layout': site_layout(),
             'page_title': page_title,
             'au': au,
             'results': results,
+            'type': type,
+            'host': host,
+            'uniq_id': uniq_id,
+            'ng': ng,
+            'vhost': vhost,
            }
 
